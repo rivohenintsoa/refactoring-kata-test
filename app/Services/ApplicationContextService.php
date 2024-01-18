bbd5f7bf8fe64a034bf030c1ca957ac7420b1ddb
rivohenintso <rivhenintsoaavotra@example.com>
2024-01-18 21:27:45 +0300

    Laravel service for Application Context.

--- app/Services/ApplicationContextService.php
+++ app/Services/ApplicationContextService.php
@@ -5,23 +5,46 @@ namespace App\Services;
 use App\Models\Site;
 use App\Models\User;
 
+/**
+ * Class ApplicationContextService
+ * @package App\Services
+ */
 class ApplicationContextService
 {
-
+    /**
+     * @var Site The current site.
+     */
     private $currentSite;
+
+    /**
+     * @var User The current user.
+     */
     private $currentUser;
 
-    protected function __construct()
+    /**
+     * ApplicationContextService constructor.
+     */
+    public function __construct()
     {
         $this->currentSite = new Site();
         $this->currentUser = new User();
     }
 
+    /**
+     * Get the current site.
+     *
+     * @return Site The current site.
+     */
     public function getCurrentSite()
     {
         return $this->currentSite;
     }
 
+    /**
+     * Get the current user.
+     *
+     * @return User The current user.
+     */
     public function getCurrentUser()
     {
         return $this->currentUser;
