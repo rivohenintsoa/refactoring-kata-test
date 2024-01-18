38d64540f5bf54febabb5b4d48999e42c582cd07
rivohenintso <rivhenintsoaavotra@example.com>
2024-01-18 21:39:43 +0300

    Custom command to generate a message

--- app/Console/Commands/Message.php
+++ app/Console/Commands/Message.php
@@ -4,10 +4,15 @@ namespace App\Console\Commands;
 
 use App\Models\Quote;
 use App\Models\Template;
-use App\Services\TemplateManagerService as TemplateManager;
+use App\Actions\TemplateManager as TemplateManager;
 use Illuminate\Console\Command;
 
-class message extends Command
+/**
+ * Class Message
+ *
+ * @package App\Console\Commands
+ */
+class Message extends Command
 {
     /**
      * The name and signature of the console command.
@@ -28,10 +33,10 @@ class message extends Command
      */
     public function handle()
     {
+        // Create a new template with placeholders
         $template = new Template([
             'id' => 1,
             'subject' => 'Your delivery to [quote:destination_name]',
-
             'content' => " Hi [user:first_name], Thanks for contacting us to deliver to [quote:destination_name].
         
             Regards,
@@ -39,13 +44,16 @@ class message extends Command
             SAYNA team "
         ]);
 
+        // Initialize the TemplateManager
         $templateManager = new TemplateManager();
 
+        // Create a new quote
         $quote = new Quote();
+
+        // Get the computed template with replaced placeholders
         $message = $templateManager->getTemplateComputed($template, ['quote' => $quote]);
 
+        // Display the generated message
         $this->info($message->subject . "\n" . $message->content);
-
-        echo "ahahahaha" . $message->subject . "\n" . $message->content;
     }
 }
